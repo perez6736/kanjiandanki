1fa962075370001db4fc737bc78711f32cbde734
perez6736 <perez6736@gmail.com>
2019-08-12 01:30:51 +0900

    text box was created with button

--- kanjiandanki/src/components/KanjiInput.js
+++ kanjiandanki/src/components/KanjiInput.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
-class HomePage extends React.Component {
+class KanjiInput extends React.Component {
+    state = {
+        japneseText: 'Enter Japanese text'
+    };
+
+    change = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    click = () => {
+        console.log("button was clicked");
+    }
+
     render() {
-        return <p> Hello! </p>
+        return(
+            <form>
+                <textarea 
+                name="japneseText"
+                placeholder='Japanese text' 
+                value={this.state.japneseText} 
+                onChange={e => this.change(e)}
+                />
+                <button 
+                type="button"
+                onClick={ this.click }>
+                    Find most common Kanji 
+                </button>
+            </form>
+        );
     }
 }
 
-export default HomePage; 
+export default KanjiInput; 
